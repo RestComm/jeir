67afa55f415e2333b2c13f58e3af829e046d6aad
mnowa <hawi81@gmail.com>
2016-01-14 20:46:25 +0100

    Updating jEIR to work with latest JAIN SLEE (v2.8.10.32) and jSS7 (v3.0.1322) releases closes #1

--- test/mapmodule/src/main/java/org/mobicents/eir/ihub/MSCSccpListener.java
+++ test/mapmodule/src/main/java/org/mobicents/eir/ihub/MSCSccpListener.java
@@ -1,5 +1,5 @@
 /*
- * TeleStax, Open Source Cloud Communications  Copyright 2012. 
+ * TeleStax, Open Source Cloud Communications  Copyright 2012.
  * and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -38,34 +38,39 @@ import org.mobicents.protocols.ss7.map.api.service.mobility.MAPDialogMobility;
 import org.mobicents.protocols.ss7.sccp.RemoteSccpStatus;
 import org.mobicents.protocols.ss7.sccp.SccpListener;
 import org.mobicents.protocols.ss7.sccp.SignallingPointStatus;
+//import org.mobicents.protocols.ss7.sccp.impl.parameter.ParameterFactoryImpl;
 import org.mobicents.protocols.ss7.sccp.message.SccpDataMessage;
 import org.mobicents.protocols.ss7.sccp.message.SccpNoticeMessage;
-import org.mobicents.protocols.ss7.sccp.parameter.GT0100;
+import org.mobicents.protocols.ss7.sccp.parameter.GlobalTitle0100;
+import org.mobicents.protocols.ss7.sccp.parameter.ParameterFactory;
 import org.mobicents.protocols.ss7.sccp.parameter.SccpAddress;
 
 public class MSCSccpListener implements SccpListener {
 
 	private static final Logger logger = Logger.getLogger(MSCSccpListener.class);
-	
+
 	private static final String GT_MSC = "553496629938";
 	private static final String GT_EIR = "553496629939";
-	
-	private static final int PC_MSC = 8;
-	private static final int PC_EIR = 9;
-	
+
+	private static final int SSN_MSC = 8;
+	private static final int SSN_EIR = 9;
+
 	private MAPProvider mapProvider;
-	
+
 	private SccpAddress thisAddress;
 	private SccpAddress remoteAddress;
-	
-	public MSCSccpListener(MAPProvider mapProvider) {
+
+	private ParameterFactory sccpParameterFactory;
+
+	public MSCSccpListener(MAPProvider mapProvider, ParameterFactory sccpParameterFactory) {
 		this.mapProvider = mapProvider;
+		this.sccpParameterFactory = sccpParameterFactory;
 	}
-	
+
 	@Override
 	public void onPcState(int arg0, SignallingPointStatus arg1, int arg2, RemoteSccpStatus arg3) {
 		logger.info("Connection created. Send checkIMEI request to EIR");
-		
+
 		try {
 			//TODO: Should read from a file the IMEIs and IMSIs to test.
 			for (int i = 0; i < 3; i++) {
@@ -78,37 +83,48 @@ public class MSCSccpListener implements SccpListener {
 			logger.error("Error sending checkImei request", e);
 		}
 	}
-	
+
 	private MAPDialogMobility setupCheckImeiRequest(String testIMEI, String testIMSI) throws MAPException {
 		MAPDialogMobility clientDialogMobility = this.mapProvider.getMAPServiceMobility()
 				.createNewDialog(getApplicationContext(), getMSCSccpAddress(), null, getEIRSccpAddress(), null);
-		
+
 		logger.info("DialogId: " + clientDialogMobility.getLocalDialogId() + ". Sending IMEI=[" + testIMEI + "]. IMSI=[" + testIMSI + "]");
 		MAPParameterFactory mapParameterFactory = this.mapProvider.getMAPParameterFactory();
 		IMEI imei = mapParameterFactory.createIMEI(testIMEI);
 		IMSI imsi = mapParameterFactory.createIMSI(testIMSI);
-	
+
 		clientDialogMobility.addCheckImeiRequest_Huawei(imei, null, null, imsi);
-		
+
 		return clientDialogMobility;
 	}
-	
+
+	private ParameterFactory getParameterFactory() {
+		/*if (this.parameterFactory == null) {
+			this.parameterFactory = new ParameterFactoryImpl();
+		}*/
+		return this.sccpParameterFactory;
+	}
+
 	private SccpAddress getMSCSccpAddress() {
 		if (this.thisAddress == null) {
-			GT0100 gt = new GT0100(0, NumberingPlan.ISDN_TELEPHONY, NatureOfAddress.INTERNATIONAL, GT_MSC);
-			this.thisAddress = new SccpAddress(RoutingIndicator.ROUTING_BASED_ON_GLOBAL_TITLE, 0, gt, PC_MSC);
+			//GT0100 gt = new GT0100(0, NumberingPlan.ISDN_TELEPHONY, NatureOfAddress.INTERNATIONAL, GT_MSC);
+			GlobalTitle0100 gt = getParameterFactory().createGlobalTitle(GT_MSC, 0, NumberingPlan.ISDN_TELEPHONY, null, NatureOfAddress.INTERNATIONAL);
+			//this.thisAddress = new SccpAddress(RoutingIndicator.ROUTING_BASED_ON_GLOBAL_TITLE, 0, gt, PC_MSC)
+			this.thisAddress = getParameterFactory().createSccpAddress(RoutingIndicator.ROUTING_BASED_ON_GLOBAL_TITLE, gt, 0, SSN_MSC);
 		}
 		return this.thisAddress;
 	}
-	
+
 	private SccpAddress getEIRSccpAddress() {
 		if (this.remoteAddress == null) {
-			GT0100 gt = new GT0100(0, NumberingPlan.ISDN_TELEPHONY, NatureOfAddress.INTERNATIONAL, GT_EIR);
-			this.remoteAddress = new SccpAddress(RoutingIndicator.ROUTING_BASED_ON_GLOBAL_TITLE, 0, gt, PC_EIR);
+			//GT0100 gt = new GT0100(0, NumberingPlan.ISDN_TELEPHONY, NatureOfAddress.INTERNATIONAL, GT_EIR);
+			GlobalTitle0100 gt = getParameterFactory().createGlobalTitle(GT_EIR, 0, NumberingPlan.ISDN_TELEPHONY, null, NatureOfAddress.INTERNATIONAL);
+			//this.remoteAddress = new SccpAddress(RoutingIndicator.ROUTING_BASED_ON_GLOBAL_TITLE, 0, gt, PC_EIR);
+			this.remoteAddress = getParameterFactory().createSccpAddress(RoutingIndicator.ROUTING_BASED_ON_GLOBAL_TITLE, gt, 0, SSN_EIR);
 		}
 		return this.remoteAddress;
 	}
-	
+
 	private MAPApplicationContext getApplicationContext() {
 		return MAPApplicationContext.getInstance(MAPApplicationContextName.equipmentMngtContext, MAPApplicationContextVersion.version2);
 	}
@@ -116,31 +132,31 @@ public class MSCSccpListener implements SccpListener {
 	@Override
 	public void onCoordRequest(int arg0, int arg1, int arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onCoordResponse(int arg0, int arg1, int arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMessage(SccpDataMessage arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onNotice(SccpNoticeMessage arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onState(int arg0, int arg1, boolean arg2, int arg3) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
